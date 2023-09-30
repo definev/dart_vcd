c4bf53c73a795b08e76b6c00fef403cc90c3a0b9
Definev <daiduong.workmail@gmail.com>
2023-10-01 02:45:48 +0700

    export code to lib

--- lib/dart_vcd.dart
+++ lib/dart_vcd.dart
@@ -3,6 +3,13 @@
 /// More dartdocs go here.
 library;
 
+export 'src/command.dart';
+export 'src/header.dart';
+export 'src/id_code.dart';
+export 'src/scope_item/scope_item.dart';
+export 'src/scope_item/scope_type.dart';
+export 'src/timescale.dart';
+export 'src/value.dart';
 export 'src/writer.dart';
 
 // TODO: Export any libraries intended for clients of this package.
